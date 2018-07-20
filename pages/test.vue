ea0facb5311a714b50a920d58e81c111abcbdc82
putimaru <kd1191681@st.kobedenshi.ac.jp>
2018-07-20 23:58:31 +0900

    ⏱️ begin make timer...

--- pages/test.vue
+++ pages/test.vue
@@ -1,10 +1,32 @@
 <template lang="pug">
+div
   h1.red Hello {{ name }}!
+  h2#time.time {{ hour }}:{{ minute }}:{{ second }}
+  table
+    tr
+      td
+        a.button--green#add-1h +1h
+      td
+        a.button--green#add-1m +1m
+      td
+        a.button--green#add-1s +1s
+    tr
+      td
+        a.button--green#add-3h +3h
+      td
+        a.button--green#add-3m +3m
+      td
+        a.button--green#add-3s +3s
 </template>
 
 <script lang="coffee">
 module.exports = data: ->
-  { name: 'World' }
+  {
+    name: 'World',
+    hour: '00',
+    minute: '00',
+    second: '00'
+  }
 </script>
 
 <style lang="sass">
