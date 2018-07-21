f9bc3250cfd4411d8e7fa51b15d38f21359a8051
putimaru <kd1191681@st.kobedenshi.ac.jp>
2018-07-21 15:15:28 +0900

    ➕ add timer plus, filters cant

--- pages/test.vue
+++ pages/test.vue
@@ -1,33 +1,56 @@
 <template lang="pug">
 div
   h1.red Hello {{ name }}!
-  h2#time.time {{ hour }}:{{ minute }}:{{ second }}
+  h2#time.time {{ hour | zeroPadding }}:{{ minute }}:{{ second }}
+
   table
-    tr
+    tr(v-for="n in 10")
       td
-        a.button--green#add-1h +1h
+        a.button--green(v-on:click="hour += n") +{{n}}h
       td
-        a.button--green#add-1m +1m
+        a.button--green(v-on:click="minute += n") +{{n}}m
       td
-        a.button--green#add-1s +1s
-    tr
-      td
-        a.button--green#add-3h +3h
-      td
-        a.button--green#add-3m +3m
-      td
-        a.button--green#add-3s +3s
+        a.button--green(v-on:click="second += n") +{{n}}s
 </template>
 
+<script>
+// module.exports = {
+//   data: function() {
+//     return {
+//       name: 'World',
+//       hour: 0,
+//       minute: 0,
+//       second: 0
+//     };
+//   }
+// };
+// filters: {
+//   zeroPadding: function (value, num) {
+//     var num = typeof num !== 'undefined' ? num : -2;
+//     return ('0' + value).slice num;
+//   }
+// }
+</script>
+
+
 <script lang="coffee">
+## data: function() { return {key: value} }
 module.exports = data: ->
   {
     name: 'World',
-    hour: '00',
-    minute: '00',
-    second: '00'
+    hour: 0,
+    minute: 0,
+    second: 0
   }
+# new Vue (filters: {
+#     zeroPadding: (value, num) ->
+#       console.log(value)
+#       num = if typeof num != 'undefined' then num else 2
+#       ('0' + value).slice num
+#   }
+# )
 </script>
+
 
 <style lang="sass">
 .red
